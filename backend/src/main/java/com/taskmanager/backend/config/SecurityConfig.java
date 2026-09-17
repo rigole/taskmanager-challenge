36d0d458793a54994f6aa26d53d1f15d6c5afd72
rigole <foplacide@gmail.com>
2026-09-17 13:54:59 +0100

    feat: update CORS configuration to allow production URL and add redirects for frontend

--- backend/src/main/java/com/taskmanager/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/taskmanager/backend/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // port par défaut de Vite
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://taskmanager-challenge-1.onrender.com/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
